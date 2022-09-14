679012ab968d3759d909b48c3791f843daf28fc5
E Baird <emilyiris.baird@gmail.com>
2022-09-14 17:16:59 -0600

    set project opacity using RGB gradient

--- ebaird/src/components/projectView.tsx
+++ ebaird/src/components/projectView.tsx
@@ -15,65 +15,54 @@ const ProjectView = (props: IProjectViewProps) => {
 
     return (
         <div>
-            <BrowserView
-                className="project-container"
-            >   
-            <div
-                onMouseEnter={() => setShowDetails(true)}
-                onMouseLeave={() => setShowDetails(false)}
-                className="project"
-            >                
-                <img
-                    src={projectDetails.imageUrl}
-                    className="project-image"
-                />
-                {showDetails && (
-                    <div
-                        // suppresses onClick action in parent element
-                        onClick={(event) => event.stopPropagation()}
-                        className="project-details"
-                    >
-                        <a 
-                            href={projectDetails.link}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <h3>{projectDetails.title}</h3>
-                        </a>
-                        <div className="project-description">{projectDetails.description}</div>
-                    </div>
-                )}
-            </div> 
-            </BrowserView>
-            <MobileView
-                className="project-container"
-            >
-                <div
+            <BrowserView className="project-container">
+                <div 
                     className="project"
-                    onClick={(event) => {
-                        // suppresses onClick action in parent element
-                        event.stopPropagation()
-                        toggleShowDetails()
+                    style={{
+                        backgroundImage: `linear-gradient(rgba(255,255,255,.15), rgba(255,255,255,.15)), url(${projectDetails.imageUrl})`,
+                        backgroundSize: "cover"
+                    }}
+                    onMouseEnter={() => setShowDetails(true)}
+                    onMouseLeave={() => setShowDetails(false)}
+                    onClick={(e) => {
+                        e.stopPropagation();
                     }}
                 >
-                    <img
-                        className={!showDetails ? "project-image" : "project-image-mobile-opacity"}
-                        src={projectDetails.imageUrl}
-                    />
                     {showDetails && (
-                        <div
-                            className="project-details"
+                        <div 
+                            className="project-description"
+                            style={{background: "linear-gradient(rgba(255,255,255,.75), rgba(255,255,255,.75))"}}
                         >
-                            <a 
-                                href={projectDetails.link}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <h3>{projectDetails.title}</h3>
-                            </a>
-                            <div className="project-description">{projectDetails.description}</div>
+                            <a href={projectDetails.link}><h2>{projectDetails.title}</h2></a>
+                            <p>{projectDetails.description}</p>
                         </div>
                     )}
+
+                </div>
+            </BrowserView>
+            <MobileView className="project-container">
+                <div
+                    className="project-mobile"
+                    style={{
+                        backgroundImage: `linear-gradient(rgba(255,255,255,.15), rgba(255,255,255,.15)), url(${projectDetails.imageUrl})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center"
+                    }}
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        toggleShowDetails();
+                    }}
+                >
+                    {showDetails && (
+                        <div
+                            className="project-description"
+                            style={{background: "linear-gradient(rgba(255,255,255,.75), rgba(255,255,255,.75))"}}
+                        >                        
+                            <a href={projectDetails.link}><h2>{projectDetails.title}</h2></a>
+                            <p>{projectDetails.description}</p>
+                        </div>
+                    )}
+
                 </div>
             </MobileView>
         </div>

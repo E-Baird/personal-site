1d19943b51dc616fc81649fd3fe90333894665c8
Emily Baird <emilyiris.baird@gmail.com>
2025-07-03 22:20:27 -0400

    spacing

--- ebaird/src/components/About.tsx
+++ ebaird/src/components/About.tsx
@@ -27,17 +27,15 @@ export const About = ({ backButtonBehaviour }: IAboutProps) => {
   const aboutBody: ReactElement = (
     <div>
       <h2>About</h2>
-      Hi, I'm Emily. I'm a full-stack developer based in Montréal, Québec,
-      Canada.
+      Hi, I'm Emily. I'm a full-stack developer based in Montréal, Québec, Canada.
       <br />
       <br />
-      By day, I work at {transitLink}, where I lead the project in charge of the
-      {webTpLink}, which involves writing Typescript and daydreaming
-      about the S-Bahn. By night, I write {zzzaLink} and also sleep.
+      By day, I work at {transitLink}, where I lead the project in charge of the {webTpLink}, which involves writing Typescript and daydreaming about the S-Bahn. By night, I write {zzzaLink} and also sleep.
       <br />
       <br />
-      My other interests include cybersecurity, film photography, computer
-      science pedagogy, language acquisition, and climbing rocks. <br /> <br />
+      My other interests include cybersecurity, film photography, computer science pedagogy, language acquisition, and climbing rocks.
+      <br />
+      <br />
     </div>
   );
 
